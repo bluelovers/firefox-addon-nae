0429eed3433781a80279df0c303a786f94b3838f
bluelovers <codelovers@users.sourceforge.net>
2015-02-13 04:04:41 +0800

    Nuke Anything Enhanced 1.1 --- 作者: Patrick Abi Salloum
    Allows you to hide almost anything via context menu 'Remove Object'.

---
+++ defaults/preferences/nuke_anything_enhanced.js
@@ -0,0 +1,2 @@
+pref("extensions.nuke_anything_enhanced.autoblink", '3px solid #f66');
+pref("extensions.{1ced4832-f06e-413f-aa14-9eb63ad40ace}.description", "chrome://nukeanything/locale/nukeanything.properties");
