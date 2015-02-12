c998c1dc107655781695b126a32315447e8e2df4
bluelovers <codelovers@users.sourceforge.net>
2015-02-13 04:16:40 +0800

    jsbeautifier.org

--- chrome/nukeanything.jar/content/nukeanything/nukeanythingOverlay.js
+++ chrome/nukeanything.jar/content/nukeanything/nukeanythingOverlay.js
@@ -16,14 +16,14 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
 var NukeAnything = {
-	RemovedObjects:Array(),
-	BlinkId:Array(),
-	blink_style:Object,
-	init: function() {
+	RemovedObjects: Array(),
+	BlinkId: Array(),
+	blink_style: Object,
+	init: function()
+	{
 		var menu = document.getElementById("contentAreaContextMenu");
-		menu.addEventListener("popupshowing",NukeAnything.showHide,false);
+		menu.addEventListener("popupshowing", NukeAnything.showHide, false);
 
 		// stick the Nuke Frame item where it ought to go
 		var menuitem = document.getElementById("nukeanything-do-nuke-frame");
@@ -32,170 +32,221 @@ var NukeAnything = {
 		menupopup.appendChild(menuitem);
 	},
 
-	showHide: function() {
-		if(gContextMenu) {
-			 var menuitem = document.getElementById("nukeanything-do-nuke");
-			 if(menuitem)
+	showHide: function()
+	{
+		if (gContextMenu)
+		{
+			var menuitem = document.getElementById("nukeanything-do-nuke");
+			if (menuitem)
 				menuitem.hidden = gContextMenu.isContentSelection();
-			 menuitem = document.getElementById("nukeanything-do-nuke-selection");
-			 if(menuitem)
+			menuitem = document.getElementById("nukeanything-do-nuke-selection");
+			if (menuitem)
 				menuitem.hidden = !gContextMenu.isContentSelection();
 
-			 menuitem = document.getElementById("nukeanything-do-leave-selection");
-			 if(menuitem)
+			menuitem = document.getElementById("nukeanything-do-leave-selection");
+			if (menuitem)
 				menuitem.hidden = !gContextMenu.isContentSelection();
 
-			 menuitem = document.getElementById("nukeanything-undo-nuke");
-			 if(menuitem)
+			menuitem = document.getElementById("nukeanything-undo-nuke");
+			if (menuitem)
 				menuitem.hidden = !(NukeAnything.RemovedObjects.length);
 
-			 // shouldn't need this since the entire frame submenu is hidden
-			 menuitem = document.getElementById("nukeanything-do-nuke-frame");
-			 if(menuitem)
+			// shouldn't need this since the entire frame submenu is hidden
+			menuitem = document.getElementById("nukeanything-do-nuke-frame");
+			if (menuitem)
 				menuitem.hidden = !NukeAnything.is_iframe();
 		}
 	},
 
-	undoNukeAnything: function() {
-		if(gContextMenu) {
-			var c = NukeAnything.RemovedObjects.length-1;
-			if (NukeAnything.RemovedObjects[c].hasOwnProperty("display")) {
-				NukeAnything.RemovedObjects[c]['obj'].style.display= NukeAnything.RemovedObjects[c]['display'];
-			} else {
-				NukeAnything.RemovedObjects[c]['obj'].style.backgroundImage= NukeAnything.RemovedObjects[c]['background-image'];
+	undoNukeAnything: function()
+	{
+		if (gContextMenu)
+		{
+			var c = NukeAnything.RemovedObjects.length - 1;
+			if (NukeAnything.RemovedObjects[c].hasOwnProperty("display"))
+			{
+				NukeAnything.RemovedObjects[c]['obj'].style.display = NukeAnything.RemovedObjects[c]['display'];
 			}
-			NukeAnything.RemovedObjects.splice(c,1);
+			else
+			{
+				NukeAnything.RemovedObjects[c]['obj'].style.backgroundImage = NukeAnything.RemovedObjects[c]['background-image'];
+			}
+			NukeAnything.RemovedObjects.splice(c, 1);
 		}
 	},
-	blink : function blink(node)	{
-		if (!node)                     return;
-		if (node.nodeType == null)     return;
-		if (node.nodeType == 3)        return;
-		if (node.documentElement)      return;
+	blink: function blink(node)
+	{
+		if (!node) return;
+		if (node.nodeType == null) return;
+		if (node.nodeType == 3) return;
+		if (node.documentElement) return;
 
 		var prefManager = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch);
-		NukeAnything.blink_style	 = prefManager.getCharPref('extensions.nuke_anything_enhanced.autoblink');
-		if (!NukeAnything.blink_style)         return;
+		NukeAnything.blink_style = prefManager.getCharPref('extensions.nuke_anything_enhanced.autoblink');
+		if (!NukeAnything.blink_style) return;
 
 
-		function setOutline(o) {
-			return function() {node.style.outline = o;}
+		function setOutline(o)
+		{
+			return function()
+			{
+				node.style.outline = o;
+			}
 		}
 
-		for(var i=1;i<7;++i)
-			setTimeout(setOutline((i%2)?NukeAnything.blink_style:'none'), i*100);
-		},
+		for (var i = 1; i < 7; ++i)
+			setTimeout(setOutline((i % 2) ? NukeAnything.blink_style : 'none'), i * 100);
+	},
 
-	doHighlight: function() {
-		if(gContextMenu) {
+	doHighlight: function()
+	{
+		if (gContextMenu)
+		{
 			var obj = gContextMenu.target;
-			if(obj) {
-				var id = setTimeout( function () {NukeAnything.blink(obj)} , 1000);
-				NukeAnything.BlinkId.push (id   );
+			if (obj)
+			{
+				var id = setTimeout(function()
+				{
+					NukeAnything.blink(obj)
+				}, 1000);
+				NukeAnything.BlinkId.push(id);
 			}
 		}
 	},
-	cancelHighlight: function() {
-		if(gContextMenu) {
+	cancelHighlight: function()
+	{
+		if (gContextMenu)
+		{
 			var obj = gContextMenu.target;
-			if(obj && NukeAnything.BlinkId) {
+			if (obj && NukeAnything.BlinkId)
+			{
 				var id = NukeAnything.BlinkId.pop();
-				if (id) {
-					clearTimeout( id);
+				if (id)
+				{
+					clearTimeout(id);
 				}
 			}
 		}
 	},
 
-	doNukeAnything: function() {
-		if(gContextMenu) {
+	doNukeAnything: function()
+	{
+		if (gContextMenu)
+		{
 			var obj = gContextMenu.target;
-			if(obj) {
-				if (obj.tagName =='BODY' || obj.tagName =='HTML') {
+			if (obj)
+			{
+				if (obj.tagName == 'BODY' || obj.tagName == 'HTML')
+				{
 					// Remove background on body in this case
 					var nodes = obj.ownerDocument.getElementsByTagName('body');
-					if (nodes && nodes[0]) {
+					if (nodes && nodes[0])
+					{
 						var c = NukeAnything.RemovedObjects.length;
-						NukeAnything.RemovedObjects[c]={'obj': nodes[0] , 'background-image' : nodes[0].style.backgroundImage} ;
+						NukeAnything.RemovedObjects[c] = {
+							'obj': nodes[0],
+							'background-image': nodes[0].style.backgroundImage
+						};
 						nodes[0].style.backgroundImage = "none";
 					}
 					nodes = obj.ownerDocument.getElementsByTagName('html');
-					if (nodes && nodes[0]) {
+					if (nodes && nodes[0])
+					{
 						var c = NukeAnything.RemovedObjects.length;
-						NukeAnything.RemovedObjects[c]={'obj': nodes[0] , 'background-image' : nodes[0].style.backgroundImage} ;
+						NukeAnything.RemovedObjects[c] = {
+							'obj': nodes[0],
+							'background-image': nodes[0].style.backgroundImage
+						};
 						nodes[0].style.backgroundImage = "none";
 					}
-				} else {
+				}
+				else
+				{
 					// go away!
 					var c = NukeAnything.RemovedObjects.length;
-					NukeAnything.RemovedObjects[c]={'obj': obj , 'display' : obj.style.display} ;
+					NukeAnything.RemovedObjects[c] = {
+						'obj': obj,
+						'display': obj.style.display
+					};
 					obj.style.display = "none";
 				}
 			}
 		}
 	},
 
-	doNukeSelection: function() {
+	doNukeSelection: function()
+	{
 		var focusedWindow = document.commandDispatcher.focusedWindow;
 		/******* Update by pat to make it compatible with firefox 1.0.3 **************/
 		//var sel = focusedWindow.__proto__.getSelection.call(focusedWindow);				   //Only works before 1.0.3
 		//var sel = focusedWindow .getSelection();											  //Only works in 1.0.3
-		var winWrapper = new XPCNativeWrapper(focusedWindow,'document', 'getSelection()');	//******
-		var docWrapper = new XPCNativeWrapper(winWrapper.document, 'title');				  //****** Should work in all version
-		var sel = winWrapper.getSelection();												   //******
+		var winWrapper = new XPCNativeWrapper(focusedWindow, 'document', 'getSelection()'); //******
+		var docWrapper = new XPCNativeWrapper(winWrapper.document, 'title'); //****** Should work in all version
+		var sel = winWrapper.getSelection(); //******
 		// boom!
 		sel.deleteFromDocument();
 		sel.collapseToStart();
 	},
 
-	doLeaveSelection: function() {
+	doLeaveSelection: function()
+	{
 		var focusedWindow = document.commandDispatcher.focusedWindow;
 		/******* Update by pat to make it compatible with firefox 1.0.3 **************/
 		//var sel = focusedWindow.__proto__.getSelection.call(focusedWindow);				   //Only works before 1.0.3
 		//var sel = focusedWindow .getSelection();											  //Only works in 1.0.3
-		var winWrapper = new XPCNativeWrapper(focusedWindow,'document', 'getSelection()');	//******
-		var docWrapper = new XPCNativeWrapper(winWrapper.document, 'title');				  //****** Should work in all version
-		var sel = winWrapper.getSelection();												   //******
+		var winWrapper = new XPCNativeWrapper(focusedWindow, 'document', 'getSelection()'); //******
+		var docWrapper = new XPCNativeWrapper(winWrapper.document, 'title'); //****** Should work in all version
+		var sel = winWrapper.getSelection(); //******
 		// boom!
-		var r=document.createRange();
+		var r = document.createRange();
 		//Remove stuff after the range in the same node
-		var current_range= sel.getRangeAt(sel.rangeCount-1);
-		r.setStart(current_range.endContainer,current_range.endOffset);
+		var current_range = sel.getRangeAt(sel.rangeCount - 1);
+		r.setStart(current_range.endContainer, current_range.endOffset);
 		r.setEndAfter(current_range.endContainer);
 		r.deleteContents();
 
 		//Remove stuff before the range in the same node
-		current_range= sel.getRangeAt(0);
-		r.setStart(current_range.startContainer,0);
-		r.setEnd(current_range.startContainer,current_range.startOffset);
+		current_range = sel.getRangeAt(0);
+		r.setStart(current_range.startContainer, 0);
+		r.setEnd(current_range.startContainer, current_range.startOffset);
 		r.deleteContents();
 
 		//Delete the nodes after the last selected element
-		var current_node ;
-		current_node = sel.getRangeAt(sel.rangeCount-1).endContainer;
+		var current_node;
+		current_node = sel.getRangeAt(sel.rangeCount - 1).endContainer;
 		//~ current_node = sel.getRangeAt(0).endContainer;
-		while (current_node.tagName !='BODY') {
-			while (delete_node= current_node.nextSibling) {
-				if (delete_node.nodeType == 1) {
-					delete_node.style.display ="none";
-				} else if (delete_node.nodeType == 3) {
-					delete_node.nodeValue='';
+		while (current_node.tagName != 'BODY')
+		{
+			while (delete_node = current_node.nextSibling)
+			{
+				if (delete_node.nodeType == 1)
+				{
+					delete_node.style.display = "none";
 				}
-				current_node= delete_node;
+				else if (delete_node.nodeType == 3)
+				{
+					delete_node.nodeValue = '';
+				}
+				current_node = delete_node;
 			}
 			current_node = current_node.parentNode;
 		}
 
 		//Delete the nodes before the first selected element
 		current_node = sel.getRangeAt(0).startContainer;
-		while (current_node.tagName !='BODY') {
-			while (delete_node= current_node.previousSibling) {
-				if (delete_node.nodeType == 1) {
-					delete_node.style.display ="none";
-				} else if (delete_node.nodeType == 3) {
-					delete_node.nodeValue='';
+		while (current_node.tagName != 'BODY')
+		{
+			while (delete_node = current_node.previousSibling)
+			{
+				if (delete_node.nodeType == 1)
+				{
+					delete_node.style.display = "none";
 				}
-				current_node= delete_node;
+				else if (delete_node.nodeType == 3)
+				{
+					delete_node.nodeValue = '';
+				}
+				current_node = delete_node;
 			}
 			current_node = current_node.parentNode;
 		}
@@ -203,33 +254,42 @@ var NukeAnything = {
 		current_node.parentNode.style.backgroundImage = "none";
 	},
 
-	checkFrames: function(frames, framedoc) {
-		if(!frames || frames.length == 0)
-		  return false;
+	checkFrames: function(frames, framedoc)
+	{
+		if (!frames || frames.length == 0)
+			return false;
 
-		for(var f=0; f<frames.length; f++) {
-			if(frames[f].contentDocument == framedoc) {
-			// go away!
+		for (var f = 0; f < frames.length; f++)
+		{
+			if (frames[f].contentDocument == framedoc)
+			{
+				// go away!
 				var c = NukeAnything.RemovedObjects.length;
-				NukeAnything.RemovedObjects[c]={'obj': frames[f] , 'display' : frames[f].style.display} ;
+				NukeAnything.RemovedObjects[c] = {
+					'obj': frames[f],
+					'display': frames[f].style.display
+				};
 				frames[f].style.display = "none";
 				return true;
 			}
 			// check to see if this document has frames in it
 			var doc = frames[f].contentDocument;
 			//~ if(NukeAnything.checkFrames(doc.getElementsByTagName("frame"), framedoc))
-				//~ return true;
-			if(NukeAnything.checkFrames(doc.getElementsByTagName("iframe"), framedoc))
+			//~ return true;
+			if (NukeAnything.checkFrames(doc.getElementsByTagName("iframe"), framedoc))
 				return true;
 		}
-	return false;
+		return false;
 	},
 
-	doNukeFrame: function() {
+	doNukeFrame: function()
+	{
 		var b = getBrowser();
-		if(gContextMenu && b) {
+		if (gContextMenu && b)
+		{
 			var framedoc = gContextMenu.target.ownerDocument;
-			if(framedoc) {
+			if (framedoc)
+			{
 				var doc = b.contentDocument;
 				//~ NukeAnything.checkFrames(doc.getElementsByTagName("frame"), framedoc);
 				NukeAnything.checkFrames(doc.getElementsByTagName("iframe"), framedoc);
@@ -237,21 +297,24 @@ var NukeAnything = {
 		}
 	},
 
-	is_iframe : function() {
+	is_iframe: function()
+	{
 		//~ return true;
 		if (!gContextMenu) return false;
 		if (!gContextMenu.inFrame) return false;
 		var b = getBrowser();
 		if (!b) return false;
 		var framedoc = gContextMenu.target.ownerDocument;
-		if(!framedoc) return false;
+		if (!framedoc) return false;
 
 		var doc = b.contentDocument;
 		var frames = doc.getElementsByTagName("iframe");
-		if(!frames || frames.length == 0) return false;
+		if (!frames || frames.length == 0) return false;
 
-		for(var f=0; f<frames.length; f++) {
-			if(frames[f].contentDocument == framedoc) {
+		for (var f = 0; f < frames.length; f++)
+		{
+			if (frames[f].contentDocument == framedoc)
+			{
 				return true;
 			}
 		}
@@ -260,4 +323,4 @@ var NukeAnything = {
 }
 
 // do the init on load
-window.addEventListener("load",NukeAnything.init, false);
+window.addEventListener("load", NukeAnything.init, false);
